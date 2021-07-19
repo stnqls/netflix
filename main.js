6ee9b37d91dc006f20e10ca4e1daecf01ac5e5c2
stnqls <stnqls3938@gmail.com>
2021-07-19 22:28:10 +0900

    slide효과

--- main.js
+++ main.js
@@ -7,11 +7,32 @@ textBtn.addEventListener('click',function() {
 })  
 */
 
+// let textArray = document.querySelectorAll('.qa-list .text');
+// let btnArray = document.querySelectorAll('.qa-list button');
+// document.querySelectorAll('.qa-list button').forEach(function (i) {
+//   i.addEventListener('click', function(e){
+//     const clickedBtnIndex = [...btnArray].indexOf(e.target);
+//     textArray[clickedBtnIndex].classList.toggle('open');
+//   })
+// });
+
 let textArray = document.querySelectorAll('.qa-list .text');
 let btnArray = document.querySelectorAll('.qa-list button');
-document.querySelectorAll('.qa-list button').forEach(function (i) {
-  i.addEventListener('click', function(e){
+btnArray.forEach((btn) => {
+  btn.addEventListener('click', e => {
     const clickedBtnIndex = [...btnArray].indexOf(e.target);
     textArray[clickedBtnIndex].classList.toggle('open');
   })
+});
+ 
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement : spyEl, //보여짐 여부를 감시할 요소를 설정
+      triggerHook : .8 
+
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
 });
